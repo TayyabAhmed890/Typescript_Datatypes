3c5b7c256d86de73ebe48cf7cc7da444ae380ba9
TayyabAhmed890 <160255633+TayyabAhmed890@users.noreply.github.com>
2024-02-27 08:34:37 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+//DataTypes..!
+//String
+var msg = "Hello! My Name is Tayyab Ahmed ";
+console.log("String:", msg);
+//Number
+var num = 12;
+var num2 = 12;
+console.log("Number:", num + num2);
+//boolean
+var value = true;
+var value2 = false;
+console.log("boolean:", value, value2);
+//null
+var data = null;
+console.log(data);
+//undefined
+var work = undefined;
+console.log(work);
+//Symbol
+var sym1 = Symbol("Unique ID");
+console.log(sym1);
+//bigint
+//1st Method use bigint
+var y = BigInt(123456789012345678);
+console.log(y);
+//bigint
+//2nd Method use bigint
+var x = 12345678910123434785n;
+console.log(x);
---
+++ index.ts
@@ -0,0 +1,50 @@
+//DataTypes..!
+
+//String
+
+let msg : string = "Hello! My Name is Tayyab Ahmed ";
+console.log("String:",msg);
+
+//Number
+
+let num : number = 12;
+let num2 : number = 12;
+console.log("Number:",num+num2);
+
+//boolean
+
+let value = true;
+let value2 = false;
+
+console.log("boolean:",value,value2);
+
+//null
+
+let data = null;
+console.log(data);
+
+//undefined
+
+let work = undefined
+console.log(work);
+
+//Symbol
+
+const sym1 = Symbol("Unique ID"); 
+console.log(sym1);
+
+
+//bigint
+
+//1st Method use bigint
+
+let y = BigInt(123456789012345678);
+console.log(y);
+
+//2nd Method use bigint
+
+let x = 12345678910123434785n;
+console.log(x);
+
+
+
